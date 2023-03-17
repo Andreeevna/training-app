2ae05d575e5a7ecbc7c51d886936f15d609a6231
Svetlana Andreevna <sandreeevna@gmail.com>
2023-03-17 19:59:24 +0300

    feat:a hamburger is made in a heder

---
+++ src/components/layout/hamburger/Hamburger.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { DiGhostSmall } from 'react-icons/di'
+import { IoClose } from 'react-icons/io5'
+
+import styles from './Hamburger.module.scss'
+import Menu from './Menu'
+
+const Hamburger = () => {
+	const [isShow, setIsShow] = useState(false)
+
+	return (
+		<div className={styles.wrapper}>
+			<button onClick={() => setIsShow(!isShow)}>
+				{isShow ? <IoClose color='white' /> : <DiGhostSmall color='white' />}
+			</button>
+			<Menu isShow={isShow} />
+		</div>
+	)
+}
+
+export default Hamburger
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { ImArrowLeft } from 'react-icons/im'
 import { useAuth } from '../../../hooks/useAuth'
+import Hamburger from '../hamburger/Hamburger'
 
-import style from './Header.module.scss'
+import styles from './Header.module.scss'
 
 const Header = ({ backlink }) => {
 	const { isAuth } = useAuth()
-	return <header className={style.header}>Header</header>
+	return (
+		<header className={styles.header}>
+			Header
+			<button onClick={() => {}}>
+				<ImArrowLeft />
+			</button>
+			<Hamburger />
+		</header>
+	)
 }
 
 export default Header
