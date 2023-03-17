2594e222284dea12f295223a7ded4ff2344fcfd0
Svetlana Andreevna <sandreeevna@gmail.com>
2023-03-17 16:43:40 +0300

    style: changes have been made to the basic styles, a favicon has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react'
-import './App.css'
-import viteLogo from '/vite.svg'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
 		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-			</div>
 			<h1>Vite + React</h1>
 			<div className='card'>
 				<button onClick={() => setCount(count => count + 1)}>
