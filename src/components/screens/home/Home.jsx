ae3fabfe1ed5ff610f667617c1e5170e663a5a41
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-03-19 15:05:42 +0300

    feat: application screens have been created, and routes have been prepared

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Layout from '../../layout/Layout'
+
+function Home() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<Layout>
+			<div className='Home'></div>
+		</Layout>
+	)
+}
+
+export default Home
