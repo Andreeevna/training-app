3e7361d4e6a637fe07019701471b6bfc83fe9488
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-03-20 14:25:15 +0300

    feat: implemented field for the form

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import Layout from '../../layout/Layout'
+import Button from '../../ui/button/Button'
 
 const Auth = () => {
+	const { register, handleSubmit } = useForm({
+		mode: ' onCHange'
+	})
+
+	const onSubmit = data => {
+		console.log(data)
+	}
+
 	return (
 		<Layout heading='Sign in' bgImage='/images/auth-bg.png'>
 			<div className='wrapper-inner-page'>
-				<input type='text' placeholder='Enter email' />
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type='text'
+						placeholder='Enter email'
+						{...register('email', {
+							require: 'Email is required'
+						})}
+					/>
+					<Button>Lets go</Button>
+				</form>
 			</div>
 		</Layout>
 	)
