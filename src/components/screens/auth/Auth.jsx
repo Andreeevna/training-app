03a7cdf976a7f4af5f9091f7370fe9333c641bc6
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-03-20 14:59:04 +0300

    feat: implemented loader

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -2,27 +2,51 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 import Layout from '../../layout/Layout'
+import Loader from '../../ui/Loader'
 import Button from '../../ui/button/Button'
+import Field from '../../ui/field/Field'
+
+const isLoading = false
+const isLoadingAuth = false
 
 const Auth = () => {
-	const { register, handleSubmit } = useForm({
-		mode: ' onCHange'
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
 	})
 
 	const onSubmit = data => {
+		// type
 		console.log(data)
 	}
 
 	return (
 		<Layout heading='Sign in' bgImage='/images/auth-bg.png'>
 			<div className='wrapper-inner-page'>
+				{true && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
-					<input
-						type='text'
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{
+							required: 'Email is required'
+						}}
+						type='email'
 						placeholder='Enter email'
-						{...register('email', {
-							require: 'Email is required'
-						})}
+					/>
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{
+							required: 'Password is required'
+						}}
+						type='password'
+						placeholder='Enter password'
 					/>
 					<Button>Lets go</Button>
 				</form>
