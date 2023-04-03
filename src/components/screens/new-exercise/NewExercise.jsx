611f8f088ff4aa7103c0535689a407d9db026451
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-03 15:22:59 +0300

    feat: implemented a service for exercises

--- src/components/screens/new-exercise/NewExercise.jsx
+++ src/components/screens/new-exercise/NewExercise.jsx
@@ -1,6 +1,10 @@
+import { useMutation } from '@tanstack/react-query'
 import React from 'react'
 
+import ExerciseServices from '../../../services/exercise/exercise.services'
+
 const NewExercise = () => {
+	const {} = useMutation(['create exercise'], () => ExerciseServices)
 	return <div>NewExercise</div>
 }
 
