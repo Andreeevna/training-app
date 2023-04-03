29195fda42f8f614b7640c0a3f477a5eb8e4f5f4
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-03 14:40:05 +0300

    feat: implemented statistics in the app

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom'
 
 import Layout from '../../layout/Layout'
 import Button from '../../ui/button/Button'
+import Statistics from '../profile/statistics/Statistics'
 
 import styles from './Home.module.scss'
 
@@ -11,7 +12,7 @@ const Home = () => {
 		<Layout bgImage='/images/home-bg.jpg'>
 			<Button clickHandler={() => navigate('/new-workout')}>New</Button>
 			<h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
-			{/* TODO: Counters */}
+			<Statistics />
 		</Layout>
 	)
 }
