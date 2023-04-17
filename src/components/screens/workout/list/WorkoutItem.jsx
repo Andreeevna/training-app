20a472034ce1b34c99e469e90a0262e213cbaa29
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-17 18:34:59 +0300

    feat: implemented an exercise creation component

--- src/components/screens/workout/list/WorkoutItem.jsx
+++ src/components/screens/workout/list/WorkoutItem.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import styles from '../detail/Workout.module.scss'
 
-const WorkoutItem = () => {
-	return <div>WorkoutItem</div>
+const WorkoutItem = ({ workout, mutate }) => {
+	return (
+		<div className={styles.item}>
+			<button
+				aria-label='Create new workout'
+				onClick={() => mutate(workout.id)}
+			>
+				<span>{workout.name}</span>
+			</button>
+		</div>
+	)
 }
 
 export default WorkoutItem
