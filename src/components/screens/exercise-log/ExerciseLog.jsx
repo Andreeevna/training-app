83a7eee87590d02bebfc17f3f12a81b8cb7f588f
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-25 21:17:33 +0300

    feat: put together the exercise log page

--- src/components/screens/exercise-log/ExerciseLog.jsx
+++ src/components/screens/exercise-log/ExerciseLog.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
 import { useExerciseLog } from './hooks/useExerciseLog'
+
+import Loader from '../../ui/Loader'
+import Alert from '../../ui/alert/Alert'
+
+import styles from './ExerciseLog.module.scss'
+import HeaderExerciseLog from './HeaderExerciseLog'
+import TableHeader from './table/TableHeader'
+import TableRow from './table/TableRow'
 
 const ExerciseLog = () => {
 	const { exerciseLog, isLoading, isSuccess } = useExerciseLog()
 
-	return <div>Exercise-log</div>
+	return (
+		<>
+			<HeaderExerciseLog exerciseLog={exerciseLog} isSuccess={isSuccess} />
+			<div
+				className='wrapper-inner-page'
+				style={{ paddingLeft: 0, paddingRight: 0 }}
+			>
+				{isLoading ? (
+					<Loader />
+				) : (
+					<div className={styles.wrapper}>
+						<TableHeader />
+						{exerciseLog?.times.map(item => (
+							<TableRow item={item} key={item.id} />
+						))}
+					</div>
+				)}
+
+				{isSuccess && exerciseLog?.times?.length === 0 && (
+					<Alert type='warning' text='Times not found' />
+				)}
+			</div>
+		</>
+	)
 }
 
 export default ExerciseLog
