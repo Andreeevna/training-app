bd5f832d62504f33f310deee8ca462b5872d383e
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-25 21:10:04 +0300

    feat: filled in the table header and its rows

--- src/components/screens/exercise-log/table/TableRow.jsx
+++ src/components/screens/exercise-log/table/TableRow.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import cn from 'clsx'
 
-const TableRow = () => {
-	return <div>TableRow</div>
+import styles from '../ExerciseLog.module.scss'
+
+const TableRow = ({ item }) => {
+	return (
+		<div
+			className={cn(styles.row, {
+				[styles.completed]: item.isCompleted
+			})}
+			key={`time ${item.id}`}
+		>
+			<div
+				className={styles.opacity}
+				key={`Prev ${item.id}/${item.prevWeight}`}
+			>
+				<input type='number' defaultValue={item.prevWeight} disabled />
+				<i>kg{item.isCompleted ? '' : ' '}/</i>
+				<input type='number' defaultValue={item.prevRepeat} disabled />
+			</div>
+
+			<div key={`RepeatWeight ${item.id}/${item.weight}`}>
+				<input
+					type='tel'
+					pattern='[0-9]*'
+					defaultValue={item.weight}
+					disabled={item.isCompleted}
+				/>
+				<i>kg{item.isCompleted && ' '}/</i>
+				<input
+					type='number'
+					defaultValue={item.repeat}
+					disabled={item.isCompleted}
+				/>
+			</div>
+
+			<div key={`Completed ${item.id}/${item.isCompleted}`}>
+				<img
+					src={
+						item.isCompleted
+							? '/images/exercises/check-completed.svg'
+							: '/images/exercises/check.svg'
+					}
+					className={styles.checkbox}
+					alt=''
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default TableRow
