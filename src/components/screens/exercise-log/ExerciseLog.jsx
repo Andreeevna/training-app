f35577f37915d90204c1eb3b95b011514c2c85f5
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-04-25 20:44:03 +0300

    feat: implemented data loading

--- src/components/screens/exercise-log/ExerciseLog.jsx
+++ src/components/screens/exercise-log/ExerciseLog.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
+import { useExerciseLog } from './hooks/useExerciseLog'
+
 const ExerciseLog = () => {
+	const { exerciseLog, isLoading, isSuccess } = useExerciseLog()
+
 	return <div>Exercise-log</div>
 }
 
